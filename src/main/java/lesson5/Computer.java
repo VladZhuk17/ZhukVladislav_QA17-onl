7b742097df8e2009fb8f9024955df67715264fcf
vladzhuk091193 <100703144+vladZhuk091193@users.noreply.github.com>
2022-08-29 17:46:25 +0200

    homeTask.lesson5

--- src/main/java/lesson5/Computer.java
+++ src/main/java/lesson5/Computer.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
      /*  Создать класс компьютер.
         Поля:
-        boolean isOn, boolean процессор, boolean оперативка, boolean жесткий диск, int ресурс полных циклов работы (включений/выключений).
+        boolean isOn, boolean процессор, boolean оперативка, boolean жесткий диск,
+        int ресурс полных циклов работы (включений/выключений).
         Методы:
         - метод описание(вывод всех полей)
         вывод:  [“есть”, “есть”, “есть”, 18 циклов]
@@ -51,47 +52,90 @@ public class Computer {
         } else {
             System.out.print("memory нет; ");
         }
-        System.out.print(resource + "]");
+        System.out.println(resource + "]");
     }
 
-    public int turnOn() {
-        if (isOn = true) {
-            if (resource > 0) {
-                System.out.println("Для запуска компьютера введите число 0 либо 1");
-                int randomNumberForTernOn = random.nextInt(2);
-                int scannerNumberForTernOn = scanner.nextInt(2);
-                if (randomNumberForTernOn == scannerNumberForTernOn) {
-                    resource--;
-                    System.out.println("Компьютер запущен, циклов осталось: " + resource);
-                    isOn = false;
-                } else {
-                    resource = 0;
-                    System.out.println("Произошел сбой, компьютер сгорел");
-                }
-            } else {
-                resource = 0;
-                System.out.println("Компьютер сгорел");
-            }
+    public boolean turnOn() {
+        if (isOn) {
+            System.out.println("The computer is turned on");
+            return false;
         }
-        return resource;
+        if (resource == 0) {
+            System.out.println("The computer burned");
+            return false;
+        }
+        if (!hasProcessor() || !hasHardDrive() || !hasMemory()) {
+            System.out.println("Check components of your computer");
+            return false;
+        }
+        if (randomAndScanner()) {
+            isOn = false;
+            System.out.println("You are lucky, you have turned on the computer!");
+            return true;
+        }
+        return true;
     }
 
-    public void turnOff() {
-        System.out.println("Для выключения компьютера введите число 0 либо 1");
-        int scannerNumberForTernOff = scanner.nextInt(2);
-        int randomNumberForTernOff = random.nextInt(2);
-        if (scannerNumberForTernOff == randomNumberForTernOff) {
-            System.out.println("Компьютер выключается");
+    public boolean hasProcessor() {
+        if (!processor) {
+            System.out.println("processor missing ");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean hasHardDrive() {
+        if (!hardDrive) {
+            System.out.println("hasHardDrive missing ");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean hasMemory() {
+        if (!memory) {
+            System.out.println("memory missing ");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean randomAndScanner() {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number 0 or 1: ");
+        if (random.nextInt(2) != scanner.nextInt()) {
+            resource = 0;
+            System.out.println("Ba-bah");
+            System.out.println("The computer has burnt");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean turnOff() {
+        if (isOn) {
+            System.out.println("The computer turn off");
+            return true;
+        }
+        if (!randomAndScanner()) {
+            resource = 0;
+            System.out.println("The computer has burnt");
+            return false;
         } else {
-            System.out.println("Компьютер не выключен, произошёл сбой");
+            resource--;
+            isOn = false;
+            System.out.println("The computer has turned off");
+            return true;
         }
     }
 
     public static void main(String[] args) {
         Computer asus = new Computer(true, true, true, 15);
         Computer apple = new Computer(false, false, true, 22);
-        apple.turnOn();
-        apple.turnOn();
-        apple.turnOff();
+
+        asus.info();
+        asus.turnOn();
+        asus.turnOff();
     }
 }
